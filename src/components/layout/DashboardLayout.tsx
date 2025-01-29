ac912d762a4c28415fcf77a0e52b4597f0d40a75
ayan-akd <ayankumar.akd@gmail.com>
2025-01-29 20:57:32 +0600

    updated the dasboard layout a little

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -1,45 +1,23 @@
-import { NavLink, Outlet, useNavigate } from "react-router-dom";
-import { Button, Layout } from "antd";
+import { Outlet } from "react-router-dom";
+import { Layout } from "antd";
 import Sidebar from "./Sidebar";
 import { Footer, Header } from "antd/es/layout/layout";
 import Navbar from "./Navbar";
-import { useAppDispatch, useAppSelector } from "../../redux/hooks";
-import { logOut, useCurrentToken } from "../../redux/features/auth/authSlice";
-import CustomFooter from "./CustomFooter";
-
 
 const { Content } = Layout;
 const DashboardLayout = () => {
-  const token = useAppSelector(useCurrentToken);
-  const navigate = useNavigate();
-  const dispatch = useAppDispatch();
-  const handleLogout = () => {
-    dispatch(logOut());
-    navigate("/login");
-  };
   return (
     <div>
       <Layout style={{ minHeight: "100%" }}>
         <Sidebar />
         <Layout>
           <Header className="pl-10 pr-2 lg:pl-0 ">
-            <div className="flex justify-around items-center gap-2">
-              <Navbar />
-              {token ? (
-                <Button onClick={handleLogout}>Logout</Button>
-              ) : (
-                <NavLink to="/login">
-                  <Button>Login</Button>
-                </NavLink>
-              )}
-            </div>
+            <Navbar />
           </Header>
           <Content style={{ margin: "24px 16px 0" }}>
             <Outlet />
           </Content>
-          <Footer style={{ textAlign: "center" }}>
-            <CustomFooter />
-          </Footer>
+          <Footer style={{ textAlign: "center" }}></Footer>
         </Layout>
       </Layout>
     </div>
--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,14 +1,14 @@
-import { Menu, Layout } from "antd";
+import { Menu, Layout, Button } from "antd";
 const { Sider } = Layout;
 import { ItemType, MenuItemType } from "antd/es/menu/interface";
-import { useAppSelector } from "../../redux/hooks";
-import { useCurrentToken } from "../../redux/features/auth/authSlice";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
+import { logOut, useCurrentToken } from "../../redux/features/auth/authSlice";
 import { verifyToken } from "../../utils/verifyToken";
 import { TUser } from "../../types";
 import sidebarItemGenerator from "../../utils/sidebarItemGenerator";
 import adminPaths from "../../routes/adminRoutes";
 import userPaths from "../../routes/userRoutes";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { RiMenuFold4Fill, RiMenuFold3Fill } from "react-icons/ri";
 import logo from "../../assets/icons/icon.png";
@@ -20,10 +20,16 @@ const userRole = {
 export default function Sidebar() {
   const [collapsed, setCollapsed] = useState(false);
   const token = useAppSelector(useCurrentToken);
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
   let user;
   if (token) {
     user = verifyToken(token) as TUser;
   }
+  const handleLogout = () => {
+    dispatch(logOut());
+    navigate("/login");
+  };
 
   let sidebarItems: ItemType<MenuItemType>[] | undefined;
   switch (user?.role) {
@@ -61,8 +67,14 @@ export default function Sidebar() {
       }}
     >
       <Link to="/" className="flex items-center gap-1">
-      <img src={logo} alt="logo" className="w-10 h-10 bg-white rounded-full" />
-        <div className="md:text-xl font-bold my-5 text-white">Pedal Paradise</div>
+        <img
+          src={logo}
+          alt="logo"
+          className="w-10 h-10 bg-white rounded-full"
+        />
+        <div className="md:text-xl font-bold my-5 text-white">
+          Pedal Paradise
+        </div>
       </Link>
       <div
         className="block lg:hidden z-50"
@@ -88,6 +100,18 @@ export default function Sidebar() {
         defaultSelectedKeys={[`My Profile`]}
         items={sidebarItems}
       />
+      {!collapsed && (
+        <div className="absolute bottom-4 left-0 w-full px-4">
+          <Button
+            type="primary"
+            danger
+            onClick={handleLogout}
+            className="w-full"
+          >
+            Logout
+          </Button>
+        </div>
+      )}
     </Sider>
   );
 }
